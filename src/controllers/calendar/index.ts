5e65ef26c21652de274ef31efbf65ddaa4266892
Juan Alvarez <juanalvarezzz.dev@gmail.com>
2025-10-02 17:21:46 -0500

    Remove calendar controllers and routes, further streamlining the API by eliminating all calendar-related functionality.

--- src/controllers/calendar/index.ts
+++
@@ -1,5 +0,0 @@
-import { createEvent } from "./create.controller";
-import { listEvents } from "./getAll.controller";
-import { deleteEvent } from "./delete.controller";
-
-export { createEvent, listEvents, deleteEvent };
